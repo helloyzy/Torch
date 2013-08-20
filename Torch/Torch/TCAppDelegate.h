6c2cc2a6da3cdf3831887307cd82b55804114c0e
Yuan Mai <ronald.mai@perficientgdc.com.cn>
2013-08-20 14:31:16 +0800

    added sliding route views

--- Torch/Torch/TCAppDelegate.h
+++ Torch/Torch/TCAppDelegate.h
@@ -14,6 +14,6 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) TCViewController *viewController;
+@property (strong, nonatomic) UIViewController *viewController;
 
 @end
