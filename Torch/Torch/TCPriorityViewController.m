2d260d10a39fb147acc683a48dbc89c220c7cb17
Bo Hu <bo.hu@perficient.com>
2013-08-21 15:13:09 +0800

    change the font of priority screen according to PSD file

--- Torch/Torch/TCPriorityViewController.m
+++ Torch/Torch/TCPriorityViewController.m
@@ -37,8 +37,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     self.storeNameLable.text = @"BOHU STORE";
-    self.storeNameLable.textColor = [UIColor colorWithRed:0.263 green:0.549 blue:0.773 alpha:1];
-    self.storeNameLable.font = [UIFont systemFontOfSize:20];
+    self.storeNameLable.textColor = [UIColor colorWithRed:0.239 green:0.435 blue:0.6 alpha:1];
+    self.storeNameLable.font = [UIFont fontWithName:@"HelveticaNeueLTCom-Md" size:17];
     tableData = [NSArray arrayWithObjects:@"Egg bendedict", @"Mushroom", @"Helloworld", nil];
 
 
@@ -63,17 +63,17 @@
 -(UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
 
     UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0,0,tableView.frame.size.width, 40)];
-    [view setBackgroundColor:[UIColor colorWithRed:0.827 green:0.945 blue:0.945 alpha:1]];
+    [view setBackgroundColor:[UIColor colorWithRed:0.816 green:0.878 blue:0.91 alpha:1]];
     
-    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10,0,tableView.frame.size.width, 40)];
-    label.text = @"Priorities";
-    label.textColor = [UIColor colorWithRed:0.18 green:0.451 blue:0.765 alpha:1];
-    label.font = [UIFont boldSystemFontOfSize:19];
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(10,8,tableView.frame.size.width, 40)];
+    label.text = @"Prioridades";
+    label.textColor = [UIColor colorWithRed:0.239 green:0.435 blue:0.6 alpha:1];
+    label.font = [UIFont fontWithName:@"HelveticaNeueLTCom-Bd" size:17];
     label.backgroundColor = [UIColor clearColor];
     [view addSubview:label];
     
     UIView *sepertor = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 2)];
-    sepertor.backgroundColor = [UIColor colorWithRed:0.2 green:0.478 blue:0.773 alpha:1];
+    sepertor.backgroundColor = [UIColor colorWithRed:0.188 green:0.376 blue:0.565 alpha:1];
     [view addSubview:sepertor];
     return view;
 }
@@ -92,7 +92,9 @@
     }
    
 	cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeueLTCom-Bd" size:17];
     cell.detailTextLabel.text = @"MR. hellow";
+    cell.textLabel.font = [UIFont fontWithName:@"HelveticaNeueLTCom-Lt" size:12];
     cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     //UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(28,0,cell.frame.size.width, cell.frame.size.height)];
     //label.text = [tableData objectAtIndex:indexPath.row];
