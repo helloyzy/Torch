19ae3ac650d78435311bc823dffb7c1db7c73676
david xu <david.xu@perficient.com>
2013-08-21 15:38:44 +0800

    add store home pages

---
+++ Torch/Torch/TCStoreHomeView.h
@@ -0,0 +1,13 @@
+//
+//  TCStoreHomeView.h
+//  Torch
+//
+//  Created by xu cheng on 8/21/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCStoreHomeView : UIViewController
+
+@end
---
+++ Torch/Torch/TCStoreHomeView.m
@@ -0,0 +1,38 @@
+//
+//  TCStoreHomeView.m
+//  Torch
+//
+//  Created by xu cheng on 8/21/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "TCStoreHomeView.h"
+
+@interface TCStoreHomeView ()
+
+@end
+
+@implementation TCStoreHomeView
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
