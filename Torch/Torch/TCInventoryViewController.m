6dbdee352439d0a7a53cad011c4a9548be7fe24e
Bo Hu <bo.hu@perficient.com>
2013-08-21 15:16:01 +0800

    add new controller for inventory

---
+++ Torch/Torch/TCInventoryViewController.m
@@ -0,0 +1,38 @@
+//
+//  TCInventoryViewController.m
+//  Torch
+//
+//  Created by bo hu on 8/21/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "TCInventoryViewController.h"
+
+@interface TCInventoryViewController ()
+
+@end
+
+@implementation TCInventoryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
