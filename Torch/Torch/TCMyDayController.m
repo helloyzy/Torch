6e894572eb3664de96761366640d06672636369a
Yuan Mai <ronald.mai@perficientgdc.com.cn>
2013-08-22 10:10:45 +0800

    data binding

--- Torch/Torch/TCMyDayController.m
+++ Torch/Torch/TCMyDayController.m
@@ -14,10 +14,14 @@
 @end
 
 @implementation TCMyDayController
+@synthesize tableView = _tableView;
+@synthesize header = _header;
+@synthesize shadow = _shadow;
 
-- (id)initWithStyle:(UITableViewStyle)style
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithStyle:style];
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
     }
@@ -28,11 +32,36 @@
 {
     [super viewDidLoad];
 
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    
+    CALayer *band = [CALayer layer];
+    band.borderWidth = 3;
+    band.frame = CGRectMake(0, _header.layer.frame.size.height-2, _header.layer.frame.size.width, 2);
+    band.borderColor = [UIColor colorWithRed:48.0/255 green:96.0/255 blue:144.0/255 alpha:1].CGColor;
+    [_header.layer addSublayer:band];
+
+    UIView *topShadowView = [[UIView alloc] initWithFrame:CGRectMake(_tableView.frame.origin.x, _tableView.frame.origin.y, _tableView.bounds.size.width, 10)];
+    CAGradientLayer *shadow = [CAGradientLayer layer];
+    shadow.frame = CGRectMake(0, 0, self.tableView.bounds.size.width, 10);
+    shadow.colors = [NSArray arrayWithObjects:(id)[[UIColor colorWithWhite:0.0 alpha:0.25f] CGColor], (id)[[UIColor clearColor] CGColor], nil];
+    [topShadowView.layer insertSublayer:shadow atIndex:0];
+    
+    [_tableView.superview addSubview:topShadowView];
+
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    NSDateFormatter *gmtFormatter=[[NSDateFormatter alloc] init];
+    [gmtFormatter setDateFormat:@"EEEE, MMM dd, YYYY"];
+
+    NSString* str=[[gmtFormatter stringFromDate: [[NSDate alloc] init]] uppercaseString];
+    _header.text = str;
 }
 
 - (void)didReceiveMemoryWarning
@@ -61,7 +90,7 @@
     if (cell.tag != 999) {
         cell.tag = 999;
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-        UILabel *storeLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0, 10.0, 280.0, 21.0)];
+        UILabel *storeLabel = [[UILabel alloc] initWithFrame:CGRectMake(20.0, 10.0, 280.0, 21.0)];
         storeLabel.adjustsFontSizeToFitWidth = NO;
         storeLabel.alpha = 1.000;
         storeLabel.autoresizesSubviews = YES;
@@ -71,12 +100,14 @@
         storeLabel.clipsToBounds = YES;
         storeLabel.contentMode = UIViewContentModeLeft;
         storeLabel.numberOfLines = 1;
+        storeLabel.backgroundColor = [UIColor clearColor];
         storeLabel.opaque = NO;
         storeLabel.shadowOffset = CGSizeMake(0.0, -1.0);
         storeLabel.text = @"Store Name Here";
         storeLabel.font = [UIFont fontWithName:@"HelveticaNeueLTCom-Bd" size:16];
 
-        UILabel *addressLabel = [[UILabel alloc] initWithFrame:CGRectMake(10.0, 30.0, 209.0, 63.0)];
+        UILabel *addressLabel = [[UILabel alloc] initWithFrame:CGRectMake(20.0, 30.0, 209.0, 63.0)];
+        addressLabel.backgroundColor = [UIColor clearColor];
         addressLabel.adjustsFontSizeToFitWidth = NO;
         addressLabel.autoresizesSubviews = YES;
         addressLabel.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin;
