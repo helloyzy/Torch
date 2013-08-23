d90e61c9e616b9d4f5ee7bdade408640572e7ff9
Bo Hu <bo.hu@perficient.com>
2013-08-23 10:35:51 +0800

    add productItemObject class which will be used as bean object for Product

---
+++ Torch/ProductItemObject.h
@@ -0,0 +1,13 @@
+//
+//  ProductItemObject.h
+//  Torch
+//
+//  Created by bo hu on 8/23/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ProductItemObject : NSObject
+
+@end
---
+++ Torch/ProductItemObject.m
@@ -0,0 +1,13 @@
+//
+//  ProductItemObject.m
+//  Torch
+//
+//  Created by bo hu on 8/23/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "ProductItemObject.h"
+
+@implementation ProductItemObject
+
+@end
