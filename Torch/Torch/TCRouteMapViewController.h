d153cf046826ebcb80bc3a010484297810d1fffc
Yuan Mai <ronald.mai@perficientgdc.com.cn>
2013-08-23 09:39:26 +0800

    map WIP

--- Torch/Torch/TCRouteMapViewController.h
+++ Torch/Torch/TCRouteMapViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
-@interface TCRouteMapViewController : UIViewController
+@interface TCRouteMapViewController : UIViewController<MKMapViewDelegate>
 
 @property (nonatomic, strong) IBOutlet MKMapView *mapView;
 
