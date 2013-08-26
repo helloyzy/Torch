30169b5f5756c789779b63d1d1cf0a72f4c9dd0c
Yuan Mai <ronald.mai@perficientgdc.com.cn>
2013-08-26 13:56:19 +0800

    call highlight cell

---
+++ Torch/Torch/TCCallHighlightCell.h
@@ -0,0 +1,13 @@
+//
+//  TCCallHighlightCell.h
+//  Torch
+//
+//  Created by Yuan Mai on 8/26/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCCallHighlightCell : UITableViewCell
+
+@end
