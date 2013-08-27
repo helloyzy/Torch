4ea37d74d75a2c63efdee7c7a69a78440e470dc3
Whitman <whitman.yang@gmail.com>
2013-08-27 13:45:39 +0800

    Add Graphics Utils for drawing utils (Core Graphics)

---
+++ Torch/Torch/GraphicsUtils.m
@@ -0,0 +1,30 @@
+//
+//  GraphicsUtils.m
+//  Torch
+//
+//  Created by Whitman Yang on 8/27/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "GraphicsUtils.h"
+
+void drawLinearGradient(CGContextRef context, CGRect rect, CGColorRef startColor, CGColorRef endColor) {
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGFloat locations[] = { 0.0, 1.0 };
+    
+    NSArray *colors = @[(__bridge id) startColor, (__bridge id) endColor];
+    
+    CGGradientRef gradient = CGGradientCreateWithColors(colorSpace, (__bridge CFArrayRef) colors, locations);
+    
+    CGPoint startPoint = CGPointMake(CGRectGetMidX(rect), CGRectGetMinY(rect));
+    CGPoint endPoint = CGPointMake(CGRectGetMidX(rect), CGRectGetMaxY(rect));
+    
+    CGContextSaveGState(context);
+    CGContextAddRect(context, rect);
+    CGContextClip(context);
+    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
+    CGContextRestoreGState(context);
+    
+    CGGradientRelease(gradient);
+    CGColorSpaceRelease(colorSpace);
+}
