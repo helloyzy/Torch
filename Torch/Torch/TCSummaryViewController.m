c7d66131200c3ee8e2f261d61d11a4b10110cc5e
Yuan Mai <ronald.mai@perficientgdc.com.cn>
2013-08-28 15:30:17 +0800

    more mgboxes for summary

--- Torch/Torch/TCSummaryViewController.m
+++ Torch/Torch/TCSummaryViewController.m
@@ -29,29 +29,27 @@
         [self.view addSubview:scroller];
         MGTableBox *section = MGTableBox.box;
         [scroller.boxes addObject:section];
-        UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 270.0, 21.0)];
-        title.adjustsFontSizeToFitWidth = NO;
-        title.alpha = 1.000;
-        title.autoresizesSubviews = YES;
-        title.autoresizingMask = UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin;
-        title.baselineAdjustment = UIBaselineAdjustmentAlignBaselines;
-        title.clearsContextBeforeDrawing = YES;
-        title.clipsToBounds = YES;
-        title.contentMode = UIViewContentModeLeft;
-        title.numberOfLines = 1;
-        title.backgroundColor = [UIColor clearColor];
-        title.opaque = NO;
-        title.shadowOffset = CGSizeMake(0.0, -1.0);
-        title.text = @"Store Name Here";
-        title.font = [UIFont fontWithName:@"HelveticaNeueLTCom-Bd" size:16];
-        MGLineStyled *header = [MGLine lineWithLeft:title right:nil size:rowSize];
-        header.borderStyle = MGBorderNone;
-        header.leftPadding = header.rightPadding = 16;
-        [section.topLines addObject:header];
-
-        MGLineStyled *row1 = [MGLineStyled lineWithLeft:@"Left text"
-                                                  right:[UIImage imageNamed:@"horse.png"] size:rowSize];
-        [section.topLines addObject:row1];
+        MGLineStyled *storeName = [MGLineStyled lineWithLeft:@"Store Name Here" right:nil size:rowSize];
+        storeName.borderStyle = MGBorderEtchedBottom;
+        storeName.bottomBorderColor = [UIColor blueColor];
+        storeName.leftPadding = storeName.rightPadding = 16;
+        
+        MGLineStyled *summaryTitle = [MGLineStyled lineWithLeft:@"Call Summary" right:nil size:rowSize];
+        
+        MGBox *summary = [MGBox boxWithSize:(CGSize) {300, 100}];
+        summary.contentLayoutMode = MGLayoutGridStyle;
+        
+        CGSize cellSize = (CGSize){ 100, 50 };
+        
+        MGLineStyled *total = [MGLineStyled lineWithLeft:@"$354" right:nil size:cellSize];
+        MGLineStyled *outOfStock = [MGLineStyled lineWithLeft:@"3" right:nil size:cellSize];
+        MGLineStyled *callTime = [MGLineStyled lineWithLeft:@"21min" right:nil size:cellSize];
+        MGLineStyled *totalTxt = [MGLineStyled lineWithLeft:@"Retail Ordered Total" right:nil size:cellSize];
+        MGLineStyled *outOfStockTxt = [MGLineStyled lineWithLeft:@"Out of Stocks Filled" right:nil size:cellSize];
+        MGLineStyled *callTimeTxt = [MGLineStyled lineWithLeft:@"Call Time" right:nil size:cellSize];
+        [summary.boxes addObjectsFromArray:@[total, outOfStock, callTime,
+         totalTxt, outOfStockTxt, callTimeTxt]];
+        [section.topLines addObjectsFromArray:@[storeName, summaryTitle, summary]];
         [scroller layout];
     }
     return self;
