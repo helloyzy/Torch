1abe57e18318337f298883157d17ae5b14a9b8ad
Bo Hu <bo.hu@perficient.com>
2013-08-28 15:31:59 +0800

    create new order controller

---
+++ Torch/Torch/TCOrderViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCOrderViewController.h
+//  Torch
+//
+//  Created by bo hu on 8/28/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCOrderViewController : UIViewController
+
+@end
---
+++ Torch/Torch/TCOrderViewController.m
@@ -0,0 +1,38 @@
+//
+//  TCOrderViewController.m
+//  Torch
+//
+//  Created by bo hu on 8/28/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "TCOrderViewController.h"
+
+@interface TCOrderViewController ()
+
+@end
+
+@implementation TCOrderViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
