002fa2f46d37179bc72dddc783503cc3e009e858
Yuan Mai <ronald.mai@perficientgdc.com.cn>
2013-08-29 16:48:12 +0800

    added _.m

--- Pods/Pods-environment.h
+++ Pods/Pods-environment.h
@@ -78,6 +78,12 @@
 #define COCOAPODS_VERSION_MINOR_TransitionKit 1
 #define COCOAPODS_VERSION_PATCH_TransitionKit 1
 
+// Underscore.m
+#define COCOAPODS_POD_AVAILABLE_Underscore_m
+#define COCOAPODS_VERSION_MAJOR_Underscore_m 0
+#define COCOAPODS_VERSION_MINOR_Underscore_m 2
+#define COCOAPODS_VERSION_PATCH_Underscore_m 1
+
 // ViewDeck
 #define COCOAPODS_POD_AVAILABLE_ViewDeck
 #define COCOAPODS_VERSION_MAJOR_ViewDeck 2
