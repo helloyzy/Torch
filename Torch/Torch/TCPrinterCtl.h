d186daac5a1972448a33fd97ddd2533de75b22f4
Whitman <whitman.yang@gmail.com>
2013-09-03 14:00:12 +0800

    Add print controller

---
+++ Torch/Torch/TCPrinterCtl.h
@@ -0,0 +1,13 @@
+//
+//  TCPrinterCtl.h
+//  Torch
+//
+//  Created by Whitman Yang on 9/3/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCPrinterCtl : UIViewController
+
+@end
