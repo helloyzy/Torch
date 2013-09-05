13fdd0c84f3b0d163db722611dae35a0329170f3
Whitman <whitman.yang@gmail.com>
2013-09-05 14:31:20 +0800

    Added new custom view

---
+++ Torch/Torch/TCAddNewCustomerVwCtl.h
@@ -0,0 +1,13 @@
+//
+//  TCAddNewCustomerVwCtl.h
+//  Torch
+//
+//  Created by Whitman Yang on 9/5/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCAddNewCustomerVwCtl : UIViewController
+
+@end
---
+++ Torch/Torch/TCAddNewCustomerVwCtl.m
@@ -0,0 +1,38 @@
+//
+//  TCAddNewCustomerVwCtl.m
+//  Torch
+//
+//  Created by Whitman Yang on 9/5/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "TCAddNewCustomerVwCtl.h"
+
+@interface TCAddNewCustomerVwCtl ()
+
+@end
+
+@implementation TCAddNewCustomerVwCtl
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
