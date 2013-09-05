d865108a3b783718a8d7de6e5d207e6a2fc1002c
Bo Hu <bo.hu@perficient.com>
2013-09-05 16:06:54 +0800

    add new table cell to represent the cell in order history details screen

---
+++ Torch/Torch/TCOrderHistoryDetailCell.h
@@ -0,0 +1,13 @@
+//
+//  TCOrderHistoryDetailCell.h
+//  Torch
+//
+//  Created by bo hu on 9/5/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCOrderHistoryDetailCell : UITableViewCell
+
+@end
---
+++ Torch/Torch/TCOrderHistoryDetailCell.m
@@ -0,0 +1,29 @@
+//
+//  TCOrderHistoryDetailCell.m
+//  Torch
+//
+//  Created by bo hu on 9/5/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "TCOrderHistoryDetailCell.h"
+
+@implementation TCOrderHistoryDetailCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
