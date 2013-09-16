4ca5c4a0934d6a914d0cb511e133a5a773a6ebea
Bo Hu <bo.hu@perficient.com>
2013-09-16 10:14:59 +0800

    append a store object

--- Torch/Torch/TCStoreHomeView.h
+++ Torch/Torch/TCStoreHomeView.h
@@ -9,11 +9,13 @@
 #import <UIKit/UIKit.h>
 #import "TCNavVwCtl.h"
 #import "TCSliderView.h"
+#import "Store.h"
 
 @interface TCStoreHomeView : TCNavVwCtl<TCSliderViewDelegate> {
     
     //__weak IBOutlet TCSliderView *sliderVw;
 }
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic,copy) Store *currentStore;
 
 @end
