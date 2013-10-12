97cfe36ea812d702cde51160fce0d70a6d76814a
Yuan Mai <ronald.mai@perficientgdc.com.cn>
2013-10-12 10:57:51 +0800

    added list

---
+++ Torch/Torch/TCSurveyListController.h
@@ -0,0 +1,13 @@
+//
+//  TCSurveyListController.h
+//  Torch
+//
+//  Created by Yuan Mai on 10/12/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "TCViewController.h"
+
+@interface TCSurveyListController : TCViewController
+
+@end
---
+++ Torch/Torch/TCSurveyListController.m
@@ -0,0 +1,38 @@
+//
+//  TCSurveyListController.m
+//  Torch
+//
+//  Created by Yuan Mai on 10/12/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "TCSurveyListController.h"
+
+@interface TCSurveyListController ()
+
+@end
+
+@implementation TCSurveyListController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
