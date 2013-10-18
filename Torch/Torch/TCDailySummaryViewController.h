aee5f08f5b739e052c6d76fe24848f45d0178dc5
Bo Hu <bo.hu@perficient.com>
2013-10-18 15:14:43 +0800

    add new daily summary view into the project per HMIA-42

---
+++ Torch/Torch/TCDailySummaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  TCDailySummaryViewController.h
+//  Torch
+//
+//  Created by bo hu on 10/18/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TCDailySummaryViewController : UIViewController
+
+@end
---
+++ Torch/Torch/TCDailySummaryViewController.m
@@ -0,0 +1,38 @@
+//
+//  TCDailySummaryViewController.m
+//  Torch
+//
+//  Created by bo hu on 10/18/13.
+//  Copyright (c) 2013 Hershey. All rights reserved.
+//
+
+#import "TCDailySummaryViewController.h"
+
+@interface TCDailySummaryViewController ()
+
+@end
+
+@implementation TCDailySummaryViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
